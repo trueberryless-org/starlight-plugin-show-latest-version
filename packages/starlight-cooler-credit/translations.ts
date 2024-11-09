c9e7df1762b73d3023a764e13b5fe667d7bc9773
trueberryless <99918022+trueberryless@users.noreply.github.com>
2024-11-09 21:04:18 +0100

    Update translations.ts

--- packages/starlight-cooler-credit/translations.ts
+++ packages/starlight-cooler-credit/translations.ts
@@ -1,12 +1,12 @@
 export const Translations = {
     en: {
-        "starlightCoolerCredit.starlight.description": "Do you want to build your own docs?",
+        "starlightCoolerCredit.starlight.description": "Do you want to build your own docs? →",
         "starlightCoolerCredit.astro.title": "Built with Astro",
-        "starlightCoolerCredit.astro.description": "Want to build your own static website?",
+        "starlightCoolerCredit.astro.description": "Want to build your own static website? →",
     },
     de: {
-        "starlightCoolerCredit.starlight.description": "Möchtest du deine eigene Dokumentation erstellen?",
+        "starlightCoolerCredit.starlight.description": "Möchtest du deine eigene Dokumentation erstellen? →",
         "starlightCoolerCredit.astro.title": "Erstellt mit Astro",
-        "starlightCoolerCredit.astro.description": "Willst du deine eigene statische Website erstellen?",
+        "starlightCoolerCredit.astro.description": "Willst du deine eigene statische Website erstellen? →",
     },
 };
