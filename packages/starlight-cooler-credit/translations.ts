8379ff83d09f664989641bb7f1139bf895e7c58a
Armand Philippot <git@armand.philippot.eu>
2024-11-09 22:30:29 +0100

    add French translation to translations.ts

--- packages/starlight-cooler-credit/translations.ts
+++ packages/starlight-cooler-credit/translations.ts
@@ -13,4 +13,11 @@ export const Translations = {
         "starlightCoolerCredit.starlight-blog.title": "Erstellt mit Starlight Blog",
         "starlightCoolerCredit.starlight-blog.description": "Willst du deine eigene Starlight Blog erstellen? →",
     },
+    fr: {
+        "starlightCoolerCredit.starlight.description": "Vous souhaitez créer votre propre documentation ? →",
+        "starlightCoolerCredit.astro.title": "Construit avec Astro",
+        "starlightCoolerCredit.astro.description": "Vous souhaitez créer votre propre site web statique ? →",
+        "starlightCoolerCredit.starlight-blog.title": "Construit avec Starlight Blog",
+        "starlightCoolerCredit.starlight-blog.description": "Vous souhaitez créer votre propre blog Starlight ? →",
+    },
 };
