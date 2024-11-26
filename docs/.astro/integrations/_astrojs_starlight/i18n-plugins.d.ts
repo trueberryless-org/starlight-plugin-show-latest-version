3af55abe69e71b8b22e04c9c8b0c2e779a4756fe
trueberryless <trueberryless@users.noreply.github.com>
2024-11-26 13:37:15 +0000

    update deployment.json container image (automated)

---
+++ docs/.astro/integrations/_astrojs_starlight/i18n-plugins.d.ts
@@ -0,0 +1,10 @@
+declare namespace StarlightApp {
+	type PluginUIStringKeys = {
+		'starlightCoolerCredit.starlight.description': string;
+		'starlightCoolerCredit.astro.title': string;
+		'starlightCoolerCredit.astro.description': string;
+		'starlightCoolerCredit.starlight-blog.title': string;
+		'starlightCoolerCredit.starlight-blog.description': string;
+	};
+	interface I18n extends PluginUIStringKeys {}
+}
---
+++ docs/.astro/types.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="astro/client" />
+/// <reference path="integrations/_astrojs_starlight/i18n-plugins.d.ts" />
+/// <reference path="astro/content.d.ts" />
